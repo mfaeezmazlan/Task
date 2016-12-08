bc55c8304d5ead2bd97a224bc7f5be5f116eb017
mfaeezmazlan <mfaeezmazlan@gmail.com>
2016-12-08 14:21:14 +0800

    08122016_add abouttask

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 		            	<span class="icon-bar"></span>
 		            	<span class="icon-bar"></span>
 	          		</button>
-	          		<a class="navbar-brand" href="#">Memo</a>
+	          		<a class="navbar-brand" href="index.php">TASK</a>
 	        	</div>
 		        <div id="navbar" class="collapse navbar-collapse">
 			          <ul class="nav navbar-nav">
 				            <li class="active"><a href="#">Home</a></li>
-				            <li><a href="#about">About</a></li>
+				            <li><a href="about.php">About</a></li>
 				            <li><a href="#contact">Contact</a></li>
 			          </ul>
 		        </div><!--/.nav-collapse -->
